c95eb262ed2ea72356a117fe303e2f11f764a41b
root <root@localhost.localdomain>
2017-06-18 22:45:48 +0000

    Bootloader added

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd $(dirname "$0")
-nasm -f elf32 kernel.asm -o ./build/kasm.o
-gcc -m32 -c kernel.c -o ./build/kc.o
-ld -m elf_i386 -T link.ld -o ./build/kernel ./build/kasm.o ./build/kc.o
+nasm -f elf32 kernel.asm -o ./build/boot/kasm.o
+gcc -m32 -c kernel.c -o ./build/boot/kc.o
+ld -m elf_i386 -T link.ld -o ./build/boot/kernel ./build/boot/kasm.o ./build/boot/kc.o
 
 if [ "$1" == "run" ];
 then
 apt-get install qemu-system-i386
-qemu-system-i386 -kernel ./build/kernel
+qemu-system-i386 -kernel ./build/boot/kernel
 fi
---
+++ isogen.sh
@@ -0,0 +1,1 @@
+genisoimage -R -b boot/grub/stage2_eltorito -no-emul-boot -input-charset utf-8 -boot-load-size 4 -boot-info-table -o OPCC-OS.iso build
